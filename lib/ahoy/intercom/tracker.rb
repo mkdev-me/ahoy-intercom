31983812beb5ca8bf3ba0d6b22a3e8d05bee2946
ulizman <ulizman@gmail.com>
2017-07-31 23:13:39 +0300

    add exception logging

--- lib/ahoy/intercom/tracker.rb
+++ lib/ahoy/intercom/tracker.rb
@@ -5,7 +5,7 @@ module Ahoy
   module Intercom
     module Tracker
       include Client
-      
+
       def track(event_name, properties = {})
         intercom.events.create(
           event_name: event_name,
@@ -14,8 +14,12 @@ module Ahoy
           created_at: Time.now.to_i,
           metadata: properties
         )
-      rescue ::Intercom::IntercomError
-        # do nothing
+      rescue ::Intercom::IntercomError => e
+        require 'logger'
+
+        logger = Logger.new(STDOUT)
+        logger.level = Logger::WARN
+        logger.warn(e.message)
       end
     end
   end
